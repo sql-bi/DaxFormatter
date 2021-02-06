5f8f6e79ce6ca9dc3cb7c5f56f620d0f5a4d9d32
Alberto Spelta <alberto.spelta@gmail.com>
2021-02-06 10:52:30 +0100

    Added test for SecurityHelpers extension methods

--- src/Dax.Formatter/Dax.Formatter.Tests/DaxFormatterClientTests.cs
+++ src/Dax.Formatter/Dax.Formatter.Tests/DaxFormatterClientTests.cs
@@ -1,9 +1,9 @@
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
-
 namespace Dax.Formatter.Tests
 {
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Xunit;
+
     public class DaxFormatterClientTests
     {
         [Theory]
