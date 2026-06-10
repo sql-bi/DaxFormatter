23f65802f5599aa683e540f878d5500bbba546ed
Alberto Spelta <albertospelta@users.noreply.github.com>
2026-06-10 17:23:02 +0200

    Switch to new api.daxformatter.com endpoint, remove redirect handling
    Update service URIs to use the new API endpoint directly. The old www.daxformatter.com URLs are still online but permanently redirect (301) to the new host. We update the service URIs to point directly to the new endpoint and remove the redirect-resolution machinery that was needed to follow those redirects at runtime.

--- src/Dax.Formatter/Client/Http/DaxFormatterHttpClient.cs
+++ src/Dax.Formatter/Client/Http/DaxFormatterHttpClient.cs
@@ -18,16 +18,11 @@
         private const string MediaTypeNamesApplicationJson = "application/json";
         private const int DaxFormatterTimeoutSeconds = 60;
 
-        private readonly HashSet<HttpStatusCode> _locationChangedStatusCodes;
         private readonly JsonSerializerOptions _serializerOptions;
-        private readonly SemaphoreSlim _initializeServiceUriSemaphore;
         private readonly SemaphoreSlim _formatSemaphore;
         private readonly HttpClient _httpClient;
         private readonly string? _application;
         private readonly string? _version;
-
-        private Uri? _daxTextFormatSingleServiceUri;
-        private Uri? _daxTextFormatMultiServiceUri;
 
         public DaxFormatterHttpClient(string? application, string? version)
         {
@@ -42,23 +37,10 @@
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNamesApplicationJson));
             _httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue(nameof(DecompressionMethods.GZip)));
             
-            _initializeServiceUriSemaphore = new SemaphoreSlim(1);
             _formatSemaphore = new SemaphoreSlim(1);
 
             _serializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
             _serializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-            
-            _locationChangedStatusCodes = new HashSet<HttpStatusCode>
-            {
-                HttpStatusCode.Moved,
-                HttpStatusCode.MovedPermanently,
-                HttpStatusCode.Found,
-                HttpStatusCode.Redirect,
-                HttpStatusCode.RedirectMethod,
-                HttpStatusCode.SeeOther,
-                HttpStatusCode.RedirectKeepVerb,
-                HttpStatusCode.TemporaryRedirect
-            };
         }
 
         public async Task<DaxFormatterResponse?> FormatAsync(DaxFormatterSingleRequest request, CancellationToken cancellationToken)
@@ -112,7 +94,7 @@
             cancellationToken.ThrowIfCancellationRequested();
 
             var json = JsonSerializer.Serialize(request, _serializerOptions);
-            var uri = await GetServiceUriAsync(request, cancellationToken).ConfigureAwait(false);
+            var uri = request.DaxTextFormatUri;
 
             using var content = new StringContent(json, Encoding.UTF8, MediaTypeNamesApplicationJson);
             using var response = await _httpClient.PostAsync(uri, content, cancellationToken).ConfigureAwait(false);
@@ -128,97 +110,8 @@
             return message;
         }
 
-        private async Task<Uri> GetServiceUriAsync(DaxFormatterRequest request, CancellationToken cancellationToken)
-        {
-            if (request is DaxFormatterMultipleRequest)
-            {
-                if (_daxTextFormatMultiServiceUri == null)
-                    _daxTextFormatMultiServiceUri = await InitializeMultiServiceUriAsync().ConfigureAwait(false);
-
-                return _daxTextFormatMultiServiceUri;
-            }
-            else if (request is DaxFormatterSingleRequest)
-            {
-                if (_daxTextFormatSingleServiceUri == null)
-                    _daxTextFormatSingleServiceUri = await InitializeSingleServiceUriAsync().ConfigureAwait(false);
-
-                return _daxTextFormatSingleServiceUri;
-            }
-            else
-            {
-                throw new NotSupportedException($"Uri not supported for { request.GetType().Name } request");
-            }
-
-            async Task<Uri> InitializeSingleServiceUriAsync()
-            {
-                if (_daxTextFormatSingleServiceUri == null)
-                {
-                    await _initializeServiceUriSemaphore.WaitAsync(CancellationToken.None).ConfigureAwait(false);
-                    try
-                    {
-                        if (_daxTextFormatSingleServiceUri == null)
-                        {
-                            using var response = await _httpClient.GetAsync(request.DaxTextFormatUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
-                            var uri = request.DaxTextFormatUri;
-
-                            if (_locationChangedStatusCodes.Contains(response.StatusCode) && response.Headers.Location != null)
-                            {
-                                uri = response.Headers.Location;
-                            }
-                            else
-                            {
-                                response.EnsureSuccessStatusCode();
-                            }
-
-                            _daxTextFormatSingleServiceUri = uri;
-                        }
-                    }
-                    finally
-                    {
-                        _initializeServiceUriSemaphore.Release();
-                    }
-                }
-
-                return _daxTextFormatSingleServiceUri;
-            }
-
-            async Task<Uri> InitializeMultiServiceUriAsync()
-            {
-                if (_daxTextFormatMultiServiceUri == null)
-                {
-                    await _initializeServiceUriSemaphore.WaitAsync(CancellationToken.None).ConfigureAwait(false);
-                    try
-                    {
-                        if (_daxTextFormatMultiServiceUri == null)
-                        {
-                            using var response = await _httpClient.GetAsync(request.DaxTextFormatUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
-                            var uri = request.DaxTextFormatUri;
-
-                            if (_locationChangedStatusCodes.Contains(response.StatusCode) && response.Headers.Location != null)
-                            {
-                                uri = response.Headers.Location;
-                            }
-                            else
-                            {
-                                response.EnsureSuccessStatusCode();
-                            }
-
-                            _daxTextFormatMultiServiceUri = uri;
-                        }
-                    }
-                    finally
-                    {
-                        _initializeServiceUriSemaphore.Release();
-                    }
-                }
-
-                return _daxTextFormatMultiServiceUri;
-            }
-        }
-
         public void Dispose()
         {
-            _initializeServiceUriSemaphore.Dispose();
             _formatSemaphore.Dispose();
             _httpClient.Dispose();
         }
